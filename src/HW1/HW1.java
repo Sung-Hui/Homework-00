a090a69101f7c32670cb032cfebffffba1203b58
SUSAN <fish00516@yahoo.com.tw>
2023-09-04 16:50:06 +0800

    Homework

---
+++ src/HW1/HW1.java
@@ -0,0 +1,80 @@
+package HW1;
+
+public class  HW1{
+	//public static void main(String[] args) {
+	//請設計一隻Java程式，計算12，6這兩個數值的和與積
+	//int num1 = 12;
+	//int num2 = 6;
+	
+	//int sum = num1 + num2;
+	//System.out.println("兩數的合為" + sum);
+	
+	//int println=num1*num2;
+	//System.out.println("兩數的積為" + println);
+	//}
+//}
+
+//請設計一隻Java程式，計算200顆蛋共是幾打幾顆？ (一打為12顆)
+	//public static void main(String[] args) {
+	  //int tota1Eggs = 200;
+	  //int dozens = tota1Eggs/12;
+	//int remainingEggs = tota1Eggs%12;
+	  //System.out.println("共" + dozens + "打" +remainingEggs + "顆");		  
+   //}
+  //}
+//請由程式算出256559秒為多少天、多少小時、多少分與多少秒
+	//public static void main(String[] args) {
+		//int num1 = 256559;
+		//int day = num1/(60*60*24);
+		//int hours = (num1 % (60*60*24))/(60*60);
+		//int minutes = (num1 % (60*60*24))/(60);
+		//int seconds = num1 % 60;
+		//System.out.println("天數:" + day);
+		//System.out.println("小時數:" + hours);
+		//System.out.println("分鐘數:" + minutes);
+		//System.out.println("秒數:" + seconds);
+	//}
+//}
+//請定義一個常數為3.1415(圓周率)，並計算半徑為5的圓面積與圓周長
+	//public static void main(String[] args) {
+		//double PI = 3.1415;
+		//double radius = 5.0;
+		//double area = PI * radius * radius;
+		//double circumference = 2 * PI * radius;
+		//System.out.println("面積" + area);
+		//System.out.println("周長" + circumference);
+	//}
+	//}
+//某人在銀行存入150萬，銀行利率為2%，如果每年利息都繼續存入銀行，請用程式計算10年後，本
+//金加利息共有多少錢 (用複利計算，公式請自行google)
+	//public static void main(String[] args) {
+	//double principal = 1500000;
+	//double annua1InterestRate = 0.02;
+	//int years = 10;
+	//for(int i =0;i < years; i++) {
+		//double interest = principal*annua1InterestRate;
+		//principal=principal+interest;
+	//}
+	//System.out.println("在" + years+"本金加利息共"+principal);
+	//}
+	//}
+//請寫一隻程式，利用System.out.println()印出以下三個運算式結果：
+//5 + 5
+//5 + ‘5’
+//5 + “5”
+//並請用註解各別說明答案的產生原因
+	//public static void main(String[] args) {
+		//int x = 5 + 5;
+		//int y = 5 + '5';
+		//String z = 5 + "5";
+		
+		//System.out.println("5 + 5結果是" + x);
+	    //都是數字所以相加
+		//System.out.println("5 + ‘5’結果是" + y);
+	    //數字加字元
+		//System.out.println("5 + “5”結果是" + z);
+		//數字加字串
+		
+	//}
+	}
+	
---
+++ src/HW1/Test4Numbers.java
@@ -0,0 +1,20 @@
+package HW1;
+
+public class Test4Numbers {
+	//輸出0~100裡的4的倍數
+	public static void main(String[] args) {
+		System.out.println("0~100裡的4的倍數");
+		
+		for(int i = 0 ; i<=100 ;i++) {
+			if(i%4==0) {
+				System.out.println(i+"");
+			}
+		}
+	
+	//輸出九九乘法表
+	//使用for迴圈+while迴圈
+	//使用for迴圈+do while迴圈
+	//使用while迴圈+do while迴圈
+
+}
+}
